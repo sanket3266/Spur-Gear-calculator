1825a497f47f86753dabb6a5ead90d15a6f0c86f
Sanket Singla <mrockdud28@gmail.com>
2022-01-02 20:56:45 +0530

    first commit

---
+++ gear.py
@@ -0,0 +1,172 @@
+
+class Spurgear():
+  from math import pi,sin
+  import numpy as np
+  import pandas as pd
+  import joblib
+  from scipy.interpolate import interp1d
+  from sklearn.preprocessing import PolynomialFeatures
+  from sklearn.linear_model import LinearRegression
+  
+  def __init__(self,condition,alpha,Power,Sigma_01,Sigma_02,Cs,N1,N2,z1,z2,E1,E2,i =0,m=1.25):
+      self.alpha = alpha
+      self.P = Power
+      self.N1 = N1
+      self.N2 = N2
+      self.z1 = z1
+      self.z2 = z2
+      self.m = m
+      self.Sigma_01 = Sigma_01
+      self.Sigma_02 = Sigma_02
+      self.i = i
+      self.condition = condition
+      self.Cs = Cs
+      self.E1 = E1
+      self.E2 = E2
+      self.__prepare__()
+
+  def __prepare__(self):
+    if self.N2 == 0:
+      if self.z2 == 0:
+        self.N2 = self.N1/self.i
+      else:
+        self.i = self.z2/self.z1
+        self.N2 = self.N1/self.i
+    elif self.z2 ==0:
+      if self.N2 == 0:
+        self.z2 = self.z1*self.i
+      else:
+        self.i = self.N1/self.N2
+        self.z2 = self.z1*self.i
+    
+  
+  def __weaker_mem__(self):
+    if self.alpha == 1:
+      self.y1 = 0.124 - 0.684/self.z1
+      self.y2 = 0.124 - 0.684/self.z2
+    elif self.alpha == 2:
+      self.y1= 0.154-0.912/self.z1
+      self.y2= 0.154-0.912/self.z2
+    elif self.alpha == 3:
+      self.y1 = 0.175 - .95/self.z1
+      self.y2 = 0.175 - .95/self.z2
+    if self.Sigma_01*self.y1 < self.Sigma_02*self.y2:
+      self.sigma = self.Sigma_01
+      self.y = self.y1
+      return True
+    else:
+      self.sigma = self.Sigma_02
+      self.y = self.y2
+      return False
+
+  def tangential_tooth_load(self):
+    v_p_m=self.pi*self.z1*self.N1/60000
+    v_g_m=self.pi*self.z2*self.N2/60000
+    weaker_member = self.__weaker_mem__()
+    if weaker_member:
+      self.teeth = self.z1
+      self.rpm = self.N1
+      self.Ft_tan = 1000*self.P*self.Cs/v_p_m
+      return self.Ft_tan
+    else:
+      self.teeth = self.z2
+      self.rpm = self.N2
+      self.Ft_tan = 1000*self.P*self.Cs/v_g_m
+      return self.Ft_tan
+
+  def lewis_tangential(self):
+    self.Ft_lewis = self.sigma * 10 * self.y * self.pi
+    return self.Ft_lewis
+
+  def m3Kv(self):
+    self.tangential_tooth_load()
+    self.lewis_tangential()
+    self.m3_Kv = self.Ft_tan/self.Ft_lewis
+    return self.m3_Kv
+  
+  def pitch_line_velocity(self):
+    self.V_m = (self.pi * self.teeth * self.rpm)/60000
+    return self.V_m
+
+  def module(self):
+    mod = [1, 1.25, 1.5, 2, 2.5, 3, 4, 5, 6, 8, 10, 12, 16, 20, 25, 32, 40, 50, 60, 80, 100]
+    self.m3Kv()
+    self.pitch_line_velocity()
+    for m in mod:
+      self.Vm = self.V_m * m
+      if self.Vm <= 8:
+        Cv = 3.05/(3.05 + self.Vm)
+      elif self.Vm <= 13:
+        Cv = 4.58/(4.58 + self.Vm)
+      elif self.Vm <= 20:
+        Cv = 6.1/(6.1 + self.Vm)
+      else:
+        Cv = 0.7625/(1.0167 + self.Vm)
+      if (m ** 3) * Cv >= self.m3_Kv:
+        break
+    self.Ft = self.Ft_lewis * Cv * m
+    self.m = m
+    self.Cv = Cv
+    self.b = 10 * m
+    self.d1 = self.z1*m
+    self.d2 = self.z2 * m
+    return self.m
+  
+  def check_for_stress(self):
+    self.module()
+    sigma_allowable = self.sigma * self.Cv
+    sigma_induced = self.Ft_tan/(self.m * 10 * self.m * self.y * self.pi * self.m)
+    if sigma_allowable > sigma_induced:
+      return 'The gears are safe'
+    else:
+      return 'Change material of gears'
+
+  def __error1__(self):
+    self.module()
+    lin_reg2 = self.joblib.load('linear_model.sav')
+    poly_reg = self.joblib.load('polynomial_preprocess.sav')
+    self.e = lin_reg2.predict(poly_reg.fit_transform(self.np.array([[self.Vm]])))[0,0]
+
+  def dynamic_fac(self):
+    er = self.pd.read_csv('Deformationfactor.csv')
+    t1 =[c for c in er.columns]
+    t1 = [float(i) for i in t1[2:]]
+    t = er.query(f'Material1 == {self.condition}').query(f'Toothform == {self.alpha}').to_numpy()
+    self.__error1__()
+    y = t[0,2:]
+    C_interp = self.interp1d(t1,y)
+    self.C = C_interp(self.e)
+    return self.C
+
+
+
+  def dynamic_load(self):
+    self.module()
+    self.dynamic_fac()
+    self.Fd= self.Ft + 20.67*self.Vm*(self.C* self.b + self.Ft)/(20.67* self.Vm+ (self.C* self.b + self.Ft)**(1/2))
+    return self.Fd
+  
+  def ang(self):
+    angles = {1:self.pi*14.5/180,2:self.pi*20/180,3:self.pi*20/180}
+    self.angle = angles[self.alpha]
+
+  def __s_fac__(self):
+    self.ang()
+    load_factor_k=self.sin(self.angle)*(1/self.E1 + 1/self.E2)/1.4
+    wear_load=self.d1*self.b*(2*self.z2/(self.z1+self.z2))
+    self.s_fac_val=(self.Fd/(wear_load*load_factor_k))**.5
+
+  def BHN(self):
+    self.module()
+    self.dynamic_load()
+    self.__s_fac__()
+    bhntable = self.pd.read_csv('S_fac&BHN.csv')
+    t = bhntable[bhntable['s_fac'] >= self.s_fac_val].iloc[0,:]
+    self.BHN1 = t['BHN']
+    self.BHN2 = t['BHN2']
+    return (self.BHN1,self.BHN2)
+
+
+  
+
+  
---
+++ project.py
@@ -0,0 +1,155 @@
+from gear import *
+def userint():
+  tmp = {}
+  base_prompt = """
+      P <Value of power in KW>
+      Z1 <Number of teeth on Pinion>
+      Z2 <Number of teeth on Gear>
+      alpha <1 for 14.5 degree involute
+              2 for 20 degrees involute
+              3 for 20 degrees Stub >
+      N1 <Speed of Pinion in RPM>
+      N2 <Speed of Gear in RPM>
+      sig1 <Allowable stress of Pinion in MPa>
+      sig2 <Allowable stress of Gear in MPa>
+      E2 <Young's Modulus of Gear in MPa>
+      E1 <Young's Modulus of Pinion in MPa>
+      Cs <Service Factor>
+      Condition <1 for Iron on Iron
+                  2 for Iron on Steel
+                  3 for Steel on Steel>
+      Quit (Quit only after all the variable below have been input)
+      (If Not given give zero)
+      Choice: """
+  feedback = ""
+  while True:
+      action = input(feedback + "\n" + base_prompt)
+      feedback = ""
+      words = action.split()
+      if len(words) > 0:
+          command = words[0]
+      else:
+          command = None
+      if command == "Quit":
+          print("Exiting...")
+          return tmp
+      elif command == "P" and len(words) == 2:
+        tmp['P']=float(words[1])
+        feedback+=f'Power entered {words[1]}Kw is added'
+
+      elif command == "Z1" and len(words) == 2:
+        tmp['Z1']=float(words[1])
+        feedback += f'Pinion teeth entered {words[1]}'
+
+      elif command == "Z2" and len(words) == 2:
+        tmp['Z2']=float(words[1])
+        feedback += f'Gear teeth entered {words[1]} '
+
+      elif command == "alpha" and len(words) == 2:
+        tmp['alpha']=int(words[1])
+        feedback += f'The option chosen for pressure angle is {words[1]}'
+      elif command == "N1" and len(words) == 2:
+        tmp['N1']=float(words[1])
+        feedback += f'Speed of pinion entered is {words[1]}'
+      elif command == "N2" and len(words) == 2:
+        tmp['N2']=float(words[1])
+        feedback += f'Speed of gear entered is {words[1]}'
+      elif command == "sig1" and len(words) == 2:
+        tmp['sig1']=float(words[1])
+        feedback += f'The allovable stress for pinion entered is {words[1]}'
+      elif command == "sig2" and len(words) == 2:
+        tmp['sig2']=float(words[1])
+        feedback += f'The allovable stress for gear entered is {words[1]}'
+      elif command == "E2" and len(words) == 2:
+        tmp['E2']=float(words[1])
+        feedback += f'The Youngs modulas for gear entered is {words[1]}'
+      elif command == "E1" and len(words) == 2:
+        tmp['E1']=float(words[1])
+        feedback += f'The Youngs modulas for pinion entered is {words[1]}'
+      elif command == "Cs" and len(words) == 2:
+        tmp['Cs']=float(words[1])
+        feedback += f'Service factor entered is {words[1]}'
+      elif command == 'Condition' and len(words) == 2:
+        tmp['Condition'] = int(words[1])
+        feedback += f'You have selected {words[1]} as the condition between gears'
+      else:
+          feedback+= "I didn't understand that. Please try again."
+
+def cal():
+  data = Spurgear(t['Condition'],
+                    t['alpha'],
+                    t['P'],
+                    t['sig1'],
+                    t['sig2'],
+                    t['Cs'],
+                    t['N1'],
+                    t['N2'],
+                    t['Z1'],
+                    t['Z2'],
+                    t['E1'],
+                    t['E2'])
+  base_prompt = """
+      a (To determine the module)
+      b (To Give the face width of the gears)
+      c (To check for strength)
+      d (to get Dynamic factor)
+      e (to get Dynamic load)
+      f (BHN of the Pinion)
+      g (Dia of pinion and Gear)
+      Quit/Q (Quit only after all the variable below have been input)
+      Choice: """
+  feedback = ""
+  while True:
+    action = input(feedback + '\n' + base_prompt)
+    feedback = ''
+    words = action.split()
+    if len(words) != 0:
+        command = words[0]
+    else:
+        command = None
+    if command == 'a':
+        t1 = data.module()
+        feedback += f'The module for the given gears is {t1} mm'
+    elif command == 'b':
+        data.module()
+        t1 = data.b
+        feedback += f'The face width of the given gears is {t1} mm'
+    elif command == 'c':
+        t1 = data.check_for_stress()
+        feedback+=t1
+    elif command == 'd':
+        t1 = data.dynamic_fac()
+        feedback+= f'The dynamic load factor of the gears is {t1}'
+    elif command == 'e':
+        t1 = data.dynamic_load()
+        feedback += f'The dynamic load of the gears is {t1}N'
+    elif command == 'f':
+        t1 = data.BHN()
+        feedback += f'The BHN of pinion is {t1[0]} and of gear is {t1[1]}'
+    elif command == 'g':
+        data.module()
+        t1 = [data.d1,data.d2]
+        feedback += f'The diameter of pinion is {t1[0]} mm and pinion is {t1[1]} mm'
+    elif command == 'Quit' or command == 'q':
+        return
+    else:
+      feedback += "I didn't understand that. Please try again."
+
+tmp = userint()
+try :
+  t = {"P": tmp['P'],
+        "Z1": tmp['Z1'],
+        "Z2": tmp['Z2'],
+        "alpha": tmp['alpha'],
+        "N1": tmp['N1'],
+        "N2": tmp['N2'],
+       "sig1": tmp['sig1'],
+        "sig2": tmp['sig2'],
+        "E2": tmp['E2'],
+        "E1": tmp['E1'],
+        "Cs": tmp['Cs'],
+        'Condition': tmp['Condition']}
+  cal()
+  print('Thanks for using our program')
+except: 
+  print("Insufficient Data")
